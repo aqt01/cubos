31e22f55ada189c922fd94f174530c8ccf7995c0
Lowell Abbott <lowell.abbott@gmail.com>
2015-06-20 04:16:14 -0400

    Testing approachs for sending post from button to server

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, session
+from flask import request
+from util import processor
+import requests
+import json
 
 app = Flask(__name__)
 """
@@ -9,19 +13,48 @@ app.config['SECRET_KEY'] = 'F34TF$($e34D';
 def home(name=None):
     return render_template('index.html',name=name)
 
-"""
-    @app.route('/signup', methods=['POST'])
-    def signup():
-        session['username'] = request._form['username']
-        session['message'] = request._form['message']
-        return redirect(url_for('message'))
+@app.route('/api/receive_colors', methods=['GET','POST'])
+def receive_colors():
+    import ipdb
+    ipdb.set_trace()
+    print request._data
+    data= request.data
+    processor.color_processor(data)            
+    return data
 
-    @app.route('/message')
-    def message():
-    if not 'username' in session:
-        return abort(403)
-        return render_template('message.html', username=session['username'], 
-        message=session['message'])
+# A Naive approach
+"""
+@app.route('/send_color_red')
+def send_red():
+    url= 'http://127.0.0.1:5000/api/receive_colors'
+    data = {'color':'RED'}
+    r = requests.post(url,data=json.dumps(data))
+    return redirect(url_for('home'))
+
+
+@app.route('/send_color_blue')
+def send_blue():
+    url= 'http://127.0.0.1:5000/api/receive_colors'
+    data = {'color':'BLUE'}
+    r = requests.post(url,data=json.dumps(data))
+    return redirect(url_for('home'))
+
+
+
+
+@app.route('/send_color_green')
+def send_green():
+    url= 'http://127.0.0.1:5000/api/receive_colors'
+    data = {'color':'GREEN'}
+    r = requests.post(url,data=json.dumps(data))
+    return redirect(url_for('home'))
+
+@app.route('/send_color_white')
+def send_whie():
+    url= 'http://127.0.0.1:5000/api/receive_colors'
+    data = {'color':'WHITE'}
+    r = requests.post(url,data=json.dumps(data))
+    return redirect(url_for('home'))
 """
 
 if __name__ == '__main__':
