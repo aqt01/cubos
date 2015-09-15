6c9e7a8c268c111c39c98391774d84ae25515158
Lowell Abbott <lowell.abbott@gmail.com>
2015-09-15 01:33:28 -0400

    Add Flash, Strobe, Fade, Smooth functions

--- app/util/processor.py
+++ app/util/processor.py
@@ -5,35 +5,44 @@ class Color_Mixer:
         self.blue = 0
         self.green = 0
         self.white = 0
-    def color_processor(self, color):
-        if color=="RED":
+
+        # Functions
+        self.flash = 0
+        self.strobe = 0
+        self.fade = 0
+        self.smooth = 0
+
+    def color_processor(self, function):
+        
+        # Color count
+        if function=="RED":
             self.red=self.red+1
 
-        elif color=="GREEN":
+        elif function=="GREEN":
             self.green=1+self.green
 
-        elif color=="BLUE":
+        elif function=="BLUE":
             self.blue=self.blue+1
     
-        elif color=="WHITE":
+        elif function=="WHITE":
             self.white=1+self.white
+ 
+         # Function count
+       
+        elif function=="FLASH":
+            self.flash=self.flash+1
         
-        print color
+        elif function=="STROBE":
+            self.strobe=1+self.strobe
+
+        elif function=="FADE":
+            self.fade=self.fade+1
+    
+        elif function=="SMOOTH":
+            self.smooth=1+self.smooth
+        
+        print function
+        print "Colors count"
         print self.red,self.blue,self.green,self.white
-
-        
-
-
-
-
-#$("button").click(function(){
-#        $.post("demo_test_post.asp",
-#                {
-#                            name: "Donald Duck",
-#                                    city: "Duckburg"
-#                                        },
-#                    function(data, status){
-#                                alert("Data: " + data + "\nStatus: " + status);
-#                                    });
-#                    });i
-        
+        print "Functions count"
+        print self.flash, self.strobe, self.fade, self.smooth
